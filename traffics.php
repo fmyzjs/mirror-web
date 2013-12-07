a125d09cc3984c1fc2f3a2c5092ca9b9f1ca7bf8
Charlie Brown <stieizc.33@gmail.com>
2013-12-07 20:53:31 +0800

    traffics.php: copy from http://solar.tuna.tsinghua.edu.cn:8000/

---
+++ traffics.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+	<title>Bandwidth Utilization of MIRRORS.TUNA.TSINGHUA.EDU.CN</title>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<meta content="noarchive" name="robots" />
+    </head>
+    <body>
+	<h1>Bandwidth Utilization of mirrors.tuna.tsinghua.edu.cn</h1>
+		<h2>Thanks to <a href="http://debian.org/">Debian GNU/Linux</a>, <a href="http://perl.org">Perl</a>, <a href="http://oss.oetiker.ch/rrdtool/">RRDTool</a>, <a href="http://ftp.isc.org/isc/cron/">cron</a>, <a href="http://erlang.org">Erlang</a>, and <a href="http://yaws.hyber.org">Yaws</a></h2>
+	<table>
+	    <tr>
+		<th>Monitor</th>
+		<th>Last Hour</th>
+		<th>Last Day</th>
+		<th>Last Week</th>
+		<th>Last Month</th>
+		<th>Last Year</th>
+	    </tr>
+	    <tr>
+		<td>TUNET</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0-year.png"/></td>
+	    </tr>
+<!--
+	    <tr>
+		<td>TUNET</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0_packets-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0_packets-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0_packets-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0_packets-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth0_packets-year.png"/></td>
+	    </tr>
+-->
+	    <tr>
+		<td>CERNET</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1-year.png"/></td>
+	    </tr>
+<!--
+	    <tr>
+		<td>CERNET</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1_packets-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1_packets-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1_packets-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1_packets-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/eth1_packets-year.png"/></td>
+	    </tr>
+-->
+
+	    <tr>
+		<td>Nginx Active Connections</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/nginx_conn-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/nginx_conn-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/nginx_conn-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/nginx_conn-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/nginx_conn-year.png"/></td>
+	    </tr>
+	    <tr>
+		<td>v6</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/v6-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/v6-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/v6-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/v6-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/v6-year.png"/></td>
+		</tr>
+		<tr>
+		<td>cernet210</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet210-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet210-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet210-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet210-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet210-year.png"/></td>
+		</tr>
+		<tr>
+		<td>cernet219</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet219-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet219-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet219-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet219-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/cernet219-year.png"/></td>
+		</tr>
+		<tr>
+		<td>Conntrack Entries</td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/conntrack_entries-hour.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/conntrack_entries-day.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/conntrack_entries-week.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/conntrack_entries-month.png"/></td>
+		<td><img src="http://solar.tuna.tsinghua.edu.cn:8000/conntrack_entries-year.png"/></td>
+		</tr>
+
+		</table>
+		</body>
+		</html>
