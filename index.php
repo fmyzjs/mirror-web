c1912489886f38387e43a03e4fb91f1826854d1b
Charlie Brown <stieizc.33@gmail.com>
2013-12-14 21:34:13 +0800

    Fix indentation, give default to switch

--- index.php
+++ index.php
@@ -1,90 +1,90 @@
 <?php 
-$navActive = 0;
-include "includes/before.php";
-include "includes/distros.php";
+  $navActive = 0;
+  include "includes/before.php";
+  include "includes/distros.php";
 ?>
 
 <div id="content" class="pure-u-1">
   <div class="padding-1-lr pure-g-r">
-    <div class="pure-u-2-3 pure-g-r">
-      <div class="pure-u-1 padding-1-lr">
-        <h2> 镜像列表 </h2>
-        <table id="mirrorlist" border="0" cellspacing="0" cellpadding="0" > 
-          <thead>
-            <tr>
-            <th class="distribution">发行版</th>
-            <th class="description">说明</th>
-            <th class="update">更新时间</th>
-            <th class="help">帮助</th>
-          </tr>
-          </thead>
+	<div class="pure-u-2-3 pure-g-r">
+	  <div class="pure-u-1 padding-1-lr">
+		<h2> 镜像列表 </h2>
+		<table id="mirrorlist" border="0" cellspacing="0" cellpadding="0" > 
+		  <thead>
+			<tr>
+			  <th class="distribution">发行版</th>
+			  <th class="description">说明</th>
+			  <th class="update">更新时间</th>
+			  <th class="help">帮助</th>
+			</tr>
+		  </thead>
 		  <tbody>
 			<?php foreach ($specs as $spec): ?>
 			<?php $info = $status[$spec[0]] ?>
-		  <tr>
-			<td class="distribution">
+			<tr>
+			  <td class="distribution">
 				<a name="<?php echo $spec[0] ?>" href="<?php echo $spec[0] ?>/" title="<?php echo $spec[1] ?>">
-				<?php echo $spec[0] ?>
+				  <?php echo $spec[0] ?>
 				</a>
-			</td>
-			<td class="description">
+			  </td>
+			  <td class="description">
 				<?php echo $spec[1] ?>
-			<td class="update">
-				<?php echo $info['stamp'] ? date('Y-m-d H:i', $info['stamp']) : '' ?>
+				<td class="update">
+				  <?php echo $info['stamp'] ? date('Y-m-d H:i', $info['stamp']) : '' ?>
 				</td>
-            <td class="help"><a href="#">点我</a></td>
-          </tr>
-			<?php endforeach ?>
-          </tbody>
-        </table>
+				<td class="help"><a href="#">点我</a></td>
+			  </tr>
+			  <?php endforeach ?>
+			</tbody>
+		  </table>
 
-      </div>
-    </div>
-    <div class="pure-u-1-3">
-      <div class="aside">
-        <div class="padding-1-lr">
-          <h2>简介</h2>
-          <div class="tuna-logo">
-            <p>Powered by <a href="http://tuna.tsinghua.edu.cn/">
-              <img src="files/logo-01.png" alt="TUNA" /></a>
-            </p>
-          </div>
-          <p> 欢迎来到清华大学开源镜像网站，它由清华大学开源镜像站管理团队维护管理。</p>
-        </div>
-      </div>
-
-
-      <div class="pure-u-1">
-        <p>本站可以在校内外通过 IPv4/IPv6 使用。
-        <br />本站域名有：</p>
-        <ul>
-          <li><a href="http://mirrors.tuna.tsinghua.edu.cn/">http://mirrors.tuna.tsinghua.edu.cn/</a> 支持 IPv4/IPv6</li>
-          <li><a href="http://mirrors.6.tuna.tsinghua.edu.cn/">http://mirrors.6.tuna.tsinghua.edu.cn/</a> 只解析 IPv6</li>
-          <li><a href="http://mirrors.4.tuna.tsinghua.edu.cn/">http://mirrors.4.tuna.tsinghua.edu.cn/</a> 只解析 IPv4</li>
-        </ul>
-
-        <p><strong>镜像使用所需配置参见 <a href="http://wiki.tuna.tsinghua.edu.cn/MirrorUseHowto">Wiki</a></strong>。</p>
-      </div>
-
-      <div class="pure-u-1">
-
-        <p>如果您有任何问题或建议，请在我们的 <a href="http://issues.tuna.tsinghua.edu.cn">issue tracker</a>
-        上提交 bug，或者访问我们的<a
-          href="https://groups.google.com/forum/#%21forum/thu-opensource-mirror-admin">Google
-          Groups</a>，或直接向 Google Groups 的邮件列表 thu-opensource-mirror-admin <span class="nospam">[SPAM]</span> AT googlegroups <span class="nospam">[SPAM]</span> DOT com 寄信。
-        </p>
-
-      </div>
-    </div>
-  </div>
-</div> <!-- end of content div -->
-<div id="footer" class="pure-u-1">
-  <div class="ack">
-    <p>本站的网络和硬件由清华大学网络工程研究中心提供支持。</p>
-  </div>
-</div> <!-- end of footer div -->
-<?php include("includes/after.php")?>
+		</div>
+	  </div>
+	  <div class="pure-u-1-3">
+		<div class="aside">
+		  <div class="padding-1-lr">
+			<h2>简介</h2>
+			<div class="tuna-logo">
+			  <p>Powered by <a href="http://tuna.tsinghua.edu.cn/">
+				<img src="files/logo-01.png" alt="TUNA" /></a>
+			  </p>
+			</div>
+			<p> 欢迎来到清华大学开源镜像网站，它由清华大学开源镜像站管理团队维护管理。</p>
+		  </div>
+		</div>
 
 
-<!-- vi: se noet ts=4: -->
-<!-- vim: se noet ts=4 sts=4 expandtab sw=4-->
+		<div class="pure-u-1">
+		  <p>本站可以在校内外通过 IPv4/IPv6 使用。
+		  <br />本站域名有：</p>
+		  <ul>
+			<li><a href="http://mirrors.tuna.tsinghua.edu.cn/">http://mirrors.tuna.tsinghua.edu.cn/</a> 支持 IPv4/IPv6</li>
+			<li><a href="http://mirrors.6.tuna.tsinghua.edu.cn/">http://mirrors.6.tuna.tsinghua.edu.cn/</a> 只解析 IPv6</li>
+			<li><a href="http://mirrors.4.tuna.tsinghua.edu.cn/">http://mirrors.4.tuna.tsinghua.edu.cn/</a> 只解析 IPv4</li>
+		  </ul>
+
+		  <p><strong>镜像使用所需配置参见 <a href="http://wiki.tuna.tsinghua.edu.cn/MirrorUseHowto">Wiki</a></strong>。</p>
+		</div>
+
+		<div class="pure-u-1">
+
+		  <p>如果您有任何问题或建议，请在我们的 <a href="http://issues.tuna.tsinghua.edu.cn">issue tracker</a>
+		  上提交 bug，或者访问我们的<a
+			href="https://groups.google.com/forum/#%21forum/thu-opensource-mirror-admin">Google
+			Groups</a>，或直接向 Google Groups 的邮件列表 thu-opensource-mirror-admin <span class="nospam">[SPAM]</span> AT googlegroups <span class="nospam">[SPAM]</span> DOT com 寄信。
+		  </p>
+
+		</div>
+	  </div>
+	</div>
+  </div> <!-- end of content div -->
+  <div id="footer" class="pure-u-1">
+	<div class="ack">
+	  <p>本站的网络和硬件由清华大学网络工程研究中心提供支持。</p>
+	</div>
+  </div> <!-- end of footer div -->
+  <?php include("includes/after.php")?>
+
+
+  <!-- vi: se noet ts=4: -->
+  <!-- vim: se noet ts=4 sts=4 expandtab sw=4-->
